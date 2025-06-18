09cafd08a64f197815874d8675bb2eb105b981ec
David Lancashire <david.lancashire@gmail.com>
2025-06-18 13:57:53 +0700

    fix: redsquare css

--- node/mods/redsquare/lib/main.template.js
+++ node/mods/redsquare/lib/main.template.js
@@ -2,10 +2,10 @@ module.exports = () => {
 	return `
     <div id="saito-container" class="saito-container">
       
-      <div class="saito-sidebar left">
+      <div class="saito-sidebar hide-scrollbar left">
       </div>
       
-      <div class="saito-main">
+      <div class="saito-main hide-scrollbar">
          <div class="redsquare-progress-banner"></div>
          <div class="tweet-container"></div>
          <div class="redsquare-intersection" id="redsquare-intersection">
@@ -14,7 +14,7 @@ module.exports = () => {
          <div class="tweet-thread-holder" id="tweet-thread-holder"></div>
       </div>
      
-      <div class="saito-sidebar right">
+      <div class="saito-sidebar hide-scrollbar right">
       </div>
       
     </div>
