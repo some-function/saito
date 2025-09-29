64ae045f7be693a1f28bf9dd6070a9b922775a3a
David Lancashire <david.lancashire@gmail.com>
2025-09-29 15:54:24 +0700

    fix: realms

--- node/lib/templates/gametemplate-src/gametemplate-players.js
+++ node/lib/templates/gametemplate-src/gametemplate-players.js
@@ -12,6 +12,11 @@ class GamePlayers {
     return this.app.keychain.returnUsername(publickey, max);
   }
 
+  returnPlayerUsername(num=1) {
+    if (num >= this.game.players.length) { return "Anon"; }
+    return this.returnUsername(this.game.players[num-1]);
+  }
+
   addPlayer(address) {
     if (address === '') {
       return 0;
