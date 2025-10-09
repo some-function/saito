1f3613c4ebc162bd5766e8b69488fc6309a154cd
sankadarshana <sankadarshana@gmail.com>
2025-10-09 09:57:00 +0530

    scripts for deploy

--- rust/saito-rust/scripts/release/deploy_service.sh
+++ rust/saito-rust/scripts/release/deploy_service.sh
@@ -1,1 +1,6 @@
-ls
+#!/usr/bin/env sh
+cp scripts/saito.service /etc/systemd/system/
+sudo sed -i "s|working_dir|$(pwd)|g" /etc/systemd/system/saito.service
+sudo systemctl daemon-reload
+sudo systemctl enable saito.service
+sudo systemctl start saito.service
--- rust/saito-rust/scripts/release/reset_node.sh
+++ rust/saito-rust/scripts/release/reset_node.sh
@@ -1,1 +1,3 @@
-ls
+#!/usr/bin/env sh
+rm -rf data/blocks
+cp config/config.template.json config/
