2859647daee1cb8ba23cfcdd989ce55613cb1684
arpee <richard@saito.tech>
2025-10-13 14:01:38 +1100

    small suggested updates to deploy scripts

--- rust/saito-rust/scripts/release/deploy_service.sh
+++ rust/saito-rust/scripts/release/deploy_service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cp scripts/saito.service /etc/systemd/system/
 sudo sed -i "s|working_dir|$(pwd)|g" /etc/systemd/system/saito.service
-cp scripts/config.template.json config/
+cp scripts/config.template.json config/config.json
 sudo systemctl daemon-reload
 sudo systemctl enable saito.service
