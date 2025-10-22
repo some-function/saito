2c17cfa40cd769c3e36ebb262b61401149be2c95
Umair Khan <umair@saito.tech>
2025-10-23 01:37:02 +0500

    assetstore: refactor

---
+++ node/mods/mixin/sql/mixin_payment_requests.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS mixin_payment_requests (
+  id            INTEGER PRIMARY KEY AUTOINCREMENT,
+  request_id    INTEGER NOT NULL,
+  requested_by  TEXT NOT NULL,
+  amount        TEXT NOT NULL,
+  tx            TEXT NOT NULL,
+  status        TEXT NOT NULL CHECK (status IN ('unpaid', 'paid')),
+  created_at    INTEGER NOT NULL,
+  updated_at    INTEGER NOT NULL
+);
