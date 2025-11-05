59d853d6230a837ade71f5f31a8d5de1974a7001
David Lancashire <david.lancashire@gmail.com>
2025-11-05 16:59:30 +0700

    fix: twilight

--- node/mods/twilight/src/cards/core/saltnegotiations.js
+++ node/mods/twilight/src/cards/core/saltnegotiations.js
@@ -41,11 +41,48 @@
             }
           } catch (err) {
         	  console.log("ERROR: please check scoring error in SALT for card: " + i);
-        	}
+          }
         }
-        
+ 
+
+    	let html = `
+      	  <div class="transparent-card-overlay hide-scrollbar">
+      	    ${this.returnCardList(discard_deck)}
+      	  </div> 
+    	`;
+      
+    	if (discard_deck.length == 0) {
+	  salert("Salt Negotiations: no cards in discard pile...");
+          twilight_self.addMove("resolve\tsaltnegotiations");
+          twilight_self.endTurn();
+	  return 0;
+    	} 
+    
+   	let cc_status = this.overlay.clickToClose;
+    	this.overlay.clickToClose = false;
+        this.overlay.show(html, (card) => { 
+	  this.overlay.clickToClose = cc_status;
+        });
+        setTimeout(() => {
+	  document.querySelectorAll('.saito-overlay .transparent-card-overlay .card').forEach((el) => {
+	    el.onclick = (e) => {
+		let action2 = e.currentTarget.id;
+		document.querySelectorAll('.saito-overlay .transparent-card-overlay .card').forEach((el) => { el.onclick = (e) => {}; });
+	  	twilight_self.overlay.clickToClose = true;
+		twilight_self.overlay.close();
+        	twilight_self.unbindBackButtonFunction();
+        	twilight_self.updateStatus("Retrieving Card...");
+          	twilight_self.game.deck[0].hand.push(action2);
+        	twilight_self.addMove("resolve\tsaltnegotiations");
+          	twilight_self.addMove("NOTIFY\t"+player.toUpperCase() +" retrieved "+twilight_self.cardToText(action2));
+          	twilight_self.addMove("undiscard\t"+action2); 
+          	twilight_self.endTurn();
+	    }
+	  });
+	}, 25);
+
+/****
         twilight_self.updateStatusAndListCards("Choose Card to Reclaim:",discard_deck,true);
-        twilight_self.addMove("resolve\tsaltnegotiations");
 
         twilight_self.hud.attachControlCallback(function(action2) {
           twilight_self.game.deck[0].hand.push(action2);
@@ -53,12 +90,12 @@
           twilight_self.addMove("undiscard\t"+action2); 
           twilight_self.endTurn();
         });
-
         twilight_self.bindBackButtonFunction(()=>{
           twilight_self.addMove("NOTIFY\t"+player.toUpperCase() +" does not retrieve card");
           twilight_self.endTurn();
         })
-                  
+****/             
+
       }
      return 0;
     }
