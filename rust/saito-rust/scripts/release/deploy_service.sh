6146f662d83f3bd6a5f9bf744ab26e9cd299bbf9
sankadarshana <sankadarshana@gmail.com>
2025-12-01 12:07:33 +0530

    fixed some bugs on sync

--- rust/saito-rust/scripts/release/deploy_service.sh
+++ rust/saito-rust/scripts/release/deploy_service.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-cp scripts/saito.service /etc/systemd/system/
+cp scripts/release/saito.service /etc/systemd/system/
 sudo sed -i "s|working_dir|$(pwd)|g" /etc/systemd/system/saito.service
-cp scripts/config.template.json config/config.json
+cp scripts/release/config.template.json config/config.json
 sudo systemctl daemon-reload
 sudo systemctl enable saito.service
+echo "Configure the config file at config/config.json and start the service !"
