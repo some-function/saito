06b64395f70096f76ee2619c008421304a27a4ec
some-function <41875756+some-function@users.noreply.github.com>
2026-01-24 12:59:06 +0100

    .

--- build/webpack.config.cjs
+++ build/webpack.config.cjs
@@ -1,233 +1,39 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-
-const webpack = require("webpack");
-
-let minimize = true;
-let devtool = undefined;
-let entrypoint = "../bundler/default/index/lite.ts";
-let outputfile = "saito.js";
-if (process.argv.includes("dev")) {
-  console.log("dev mode source not minified");
-  minimize = false;
-  devtool = "eval";
-}
-if (process.argv.includes("web3")) {
-  entrypoint = "../bundler/default/index/lite-web3index.ts";
-  outputfile = "web3saito.js";
-}
-webpack(
-  {
-    cache: {
-      type: "filesystem",
-    },
-    optimization: {
-      minimize: minimize,
-      minimizer: [
-        new TerserPlugin({
-          parallel: true,
-        }),
-      ],
-      splitChunks: {
-        chunks: "async",
-        minSize: 20000,
-        minRemainingSize: 0,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
-    },
-    target: "web",
-    externals: [
-      {
-        archiver: "archiver",
-      },
-      {
-        child_process: "child_process",
-      },
-      {
-        nodemailer: "nodemailer",
-      },
-      {
-        jimp: "jimp",
-      },
-      {
-        "image-resolve": "image-resolver",
-      },
-      {
-        sqlite: "sqlite",
-      },
-      {
-        unzipper: "unzipper",
-      },
-      {
-        webpack: "webpack",
-      },
-      {
-        "node-turn": "node-turn",
-      },
-      /\.txt /,
-      /\.png$/,
-      /\.jpg$/,
-      /\.html$/,
-      /\.css$/,
-      /\.sql$/,
-      /\.md$/,
-      /\.pdf$/,
-      /\.sh$/,
-      /\.zip$/,
-      /\/web\//,
-      /\/www\//,
-    ],
-    entry: ["babel-polyfill", path.resolve(__dirname, entrypoint)],
-    output: {
-      path: path.resolve(__dirname, "./../web/saito"),
-      filename: outputfile,
-    },
-    resolve: {
-      extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ],
-      fallback: {
-        fs: false,
-        tls: false,
-        net: false,
-        path: require.resolve("path-browserify"),
-        zlib: false,
-        http: false,
-        https: false,
-        stream: require.resolve("stream-browserify"),
-        buffer: require.resolve("buffer"),
-        crypto: require.resolve("crypto-browserify"),
-        "crypto-browserify": require.resolve("crypto-browserify"),
-      },
-    },
-    experiments: {
-      asyncWebAssembly: true,
-      syncWebAssembly: true,
-      topLevelAwait: true
-    },
-    
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          exclude: /(node_modules)/,
-          use: [{
-            loader: 'ts-loader',
-            options: {
-                configFile:path.resolve(__dirname, "../build/tsconfig.json")
-            },
-            
-        }],
-        },
-        {
-          test: /\.js$/,
-          use: [
-            "source-map-loader",
-            {
-              loader: "babel-loader",
-              options: {
-                root: path.resolve(__dirname, './build'),
-                rootMode: "upward",
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-                sourceMaps: false,
-                cacheCompression: false,
-                cacheDirectory: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.mjs$/,
-          exclude: /(node_modules)/,
-          type: "javascript/auto",
-        },
-        {
-          test: /html$/,
-          exclude: [/(mods)/, /(email)/],
-        },
-        {
-          test: /quirc\.js$/,
-          loader: "exports-loader",
-        },
-        {
-          test: /\.wasm$/,
-           type: "asset/resource",
-           generator: {
-             filename: 'static/zkey/[name][hash][ext]'
-           }
-        },
-        {
-          test: /\.zkey$/,
-          type: "asset/resource",
-          generator: {
-            filename: 'static/zkey/[name][hash][ext]'
-          }
-        },
-      
-        {
-          test: /\.ptau$/,
-          type: "asset/resource",
-          generator: {
-            filename: 'static/zkey/[name][hash][ext]'
-          }
-        },
-        {
-          test: /\.circom$/,
-          type: "asset/resource",
-          generator: {
-            filename: 'static/zkey/[name][hash][ext]'
-          }
-        },
-     
-        {
-          test: /\.zip$/,
-          exclude: [
-            path.resolve(__dirname, "../mods/devtools/bundler"),
-            path.resolve(__dirname, "../mods/devtools/mods"),
-          ],
-        },
-        { 
-          test: /\.m?js/, 
-          resolve: { 
-            fullySpecified: false 
-          } 
-        }
-      ],
-
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      }),
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-      }),
-    ],
- 
-    mode: "production",
-    devtool: devtool,
-  },
-  (err, stats) => {
-    if (err || stats.hasErrors()) {
-      console.log(err);
-      if (stats) {
-        let info = stats.toJson();
-        console.log(info.errors);
+const webpack = require("webpack");  const path = require("path");  const relPath = (absPath) => path.resolve(__dirname, absPath);
+const resolve = require.resolve;  const babelPresets = ["@babel/preset-env", ["@babel/preset-react", {runtime: "automatic"}]];
+const babelOptions = {root: relPath("./build"), rootMode: "upward", presets: babelPresets, sourceMaps: false, cacheCompression: false, cacheDirectory: true};
+webpack({
+  output: {path: relPath("./../web/saito"), filename: "saito.js"},
+  entry: ["babel-polyfill", relPath("../bundler/default/index/lite.ts")], mode: "production", target: "web", cache: {type: "filesystem"},
+  experiments: {asyncWebAssembly: true, syncWebAssembly: true, topLevelAwait: true}, devtool: process.argv.includes("dev") ? "eval" : undefined,
+  plugins: [new webpack.ProvidePlugin({Buffer: ["buffer", "Buffer"]}), new webpack.ProvidePlugin({process: "process/browser"})],
+  optimization: {
+    minimize: !process.argv.includes("dev"), minimizer: [new (require("terser-webpack-plugin"))({parallel: true})],
+    splitChunks: {
+      chunks: "async", minSize: 20000, minRemainingSize: 0, minChunks: 1, maxAsyncRequests: 30, maxInitialRequests: 30, enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {priority: -10, reuseExistingChunk: true, test: /[\\/]node_modules[\\/]/},
+        default:        {priority: -20, reuseExistingChunk: true, minChunks: 2},
       }
     }
-    console.log("Bundle Success!");
-  }
-);
+  },
+  externals: [
+    ...["archiver", "child_process", "nodemailer", "jimp", "sqlite", "unzipper", "webpack", "node-turn"].map((p) => Object.fromEntries([[p, p]])),
+    {"image-resolve": "image-resolver"}, /\.(txt|png|jpg|html|css|sql|md|pdf|sh|zip)$/, /\/(web|www)\//
+  ],
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".jsx"],
+    fallback: {
+      fs: false, tls: false, net: false, zlib: false, path: resolve("path-browserify"), "crypto-browserify": resolve("crypto-browserify"),
+      http: false, https: false, stream: resolve("stream-browserify"), buffer: resolve("buffer"), crypto: resolve("crypto-browserify")
+    }
+  },
+  module: {rules: [
+    {test: /\.tsx?$/, exclude: /(node_modules)/, use: [{loader: "ts-loader", options: {configFile: relPath("../build/tsconfig.json")}}]},
+    {test: /\.jsx?$/, use: ["source-map-loader", {loader: "babel-loader", options: babelOptions}]}, {test: /html$/, exclude: [/(mods)/, /(email)/]},
+    {test: /\.mjs$/, exclude: /(node_modules)/, type: "javascript/auto"}, {test: /\.m?js/, resolve: {fullySpecified: false}},
+    {test: /\.(wasm|zkey|ptau|circom)$/, type: "asset/resource", generator: {filename: "static/zkey/[name][hash][ext]"}},
+    {test: /\.zip$/, exclude: ["bundler", "mods"].map((dir) => relPath(`../mods/devtools/${dir}`))}, {test: /quirc\.js$/, loader: "exports-loader"}
+  ]}
+}, (err, stats) => {
+  if (err || stats.hasErrors()) { console.log(err); if (stats) { console.log(stats.toJson().errors); } } else { console.log("Bundle Success!"); }
+});
